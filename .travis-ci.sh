f3987f39a73e7355961badf543ff3e443b4e83ca
Charlie Crisp <charliecrisp249@gmail.com>
2017-11-01 10:44:32 +0000

    Support travis-ci

---
+++ .travis-ci.sh
@@ -0,0 +1,16 @@
+# Edit this for your own project dependencies
+OPAM_DEPENDS="ocamlfind ounit re jbuilder"
+	 
+echo "yes" | sudo add-apt-repository ppa:avsm/ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+
+export OPAMYES=1
+opam init 
+opam install ${OPAM_DEPENDS}
+eval `opam config env`
+
+#ocamlfind list
+
+jbuilder build testfile.exe
+_build/default/testfile.exe
