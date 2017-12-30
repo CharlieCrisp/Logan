97ae8c7f6b9c7165d79b59e9eaecab43170f632d
Charlie Crisp <charliecrisp249@gmail.com>
2017-12-30 11:40:12 +0000

    Update .travis-ci.sh

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,7 +2,7 @@
 PURPLE='\033[1;35m'
 NC='\033[0m'
 
-OPAM_DEPENDS="ocamlfind ounit re jbuilder ezirmin lwt cohttp cohttp-lwt-unix"
+OPAM_DEPENDS="ocamlfind ounit re jbuilder ezirmin lwt"
 	 
 echo "yes" | sudo add-apt-repository ppa:avsm/ppa
 sudo apt-get update -qq
@@ -14,10 +14,5 @@ opam install ${OPAM_DEPENDS}
 eval `opam config env`
 
 echo -e "${PURPLE}Starting Compilation${NC}"
-jbuilder build src/Leader/HttpExampleServer.exe
-jbuilder build src/Shared/TransactionRequest.ml
+jbuilder build src/Leader/LogCreator.exe
 echo -e "${PURPLE}Completed Compilation${NC}"
-
-# echo -e "${PURPLE}Starting Unit Tests${NC}"
-# _build/default/src/Leader/LeaderServer.exe
-# echo -e "${PURPLE}Completed Unit Tests${NC}"
