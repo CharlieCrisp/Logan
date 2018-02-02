8b21dc39447d7f7f0ca730aca4af24f4ee52b650
Charlie Crisp <charliecrisp249@gmail.com>
2018-02-02 21:44:29 +0000

    Add Colours to ParticipantRunner

--- src/Participant/ParticipantRunner.ml
+++ src/Participant/ParticipantRunner.ml
@@ -4,7 +4,7 @@ module IrminLogMem = Ezirmin.FS_log(Tc.String)
 let run = Lwt_main.run
 
 (*For remote repos...*)
-let root = run (Lwt_io.write Lwt_io.stdout "\nPlease input the destination repo string: " >>= fun _ ->
+let root = run (Lwt_io.write Lwt_io.stdout "\n\027[93mPlease input the destination repo string: \027[39m" >>= fun _ ->
   Lwt_io.read_line Lwt_io.stdin)
 let memPoolMasterBranch = Lwt_main.run (IrminLogMem.init ~root:root ~bare: true () >>= IrminLogMem.master)
 let path = []
@@ -16,19 +16,19 @@ let addTransactionToMemPool senderID receiverID bookID =
   IrminLogMem.merge wipBranch ~into:memPoolMasterBranch;;
 
 let getLogEntryTuple () = 
-  Lwt_io.write Lwt_io.stdout "\nYour ID (sender): " >>= fun _ ->
+  Lwt_io.write Lwt_io.stdout "\n\027[39mYour ID (sender): \027[39m" >>= fun _ ->
   Lwt_io.read_line Lwt_io.stdin >>= fun senderID ->
-  Lwt_io.write Lwt_io.stdout "Their ID (receiver): " >>= fun _ ->
+  Lwt_io.write Lwt_io.stdout "\027[39mTheir ID (receiver): \027[39m" >>= fun _ ->
   Lwt_io.read_line Lwt_io.stdin >>= fun receiverID ->
-  Lwt_io.write Lwt_io.stdout "Item ID (book): " >>= fun _ ->
+  Lwt_io.write Lwt_io.stdout "\027[39mItem ID (book): \027[39m" >>= fun _ ->
   Lwt_io.read_line Lwt_io.stdin >>= fun bookID ->
   Lwt.return (senderID, receiverID, bookID)
 
 let rec startParticipant () = 
-  Lwt_io.write_line Lwt_io.stdout "-----------------------------------\n-----Enter Transaction Details-----" >>= fun _ ->
+  Lwt_io.write_line Lwt_io.stdout "\027[35m-----------------------------------\n-----Enter Transaction Details-----\027[39m" >>= fun _ ->
   getLogEntryTuple() >>= fun (sender, receiver, book) ->
   addTransactionToMemPool sender receiver book >>= fun _ ->
-  Lwt_io.write_line Lwt_io.stdout "\nITEM ADDED SUCCESSFULLY\n" >>= 
+  Lwt_io.write_line Lwt_io.stdout "\n\027[32mITEM ADDED SUCCESSFULLY\n" >>= 
   startParticipant;;
 
 run @@ startParticipant();;
