a4d11fc85803d7669e6317db76903740557b1528
Charlie Crisp <charliecrisp249@gmail.com>
2018-02-03 13:15:43 +0000

    Make participant set up easier

--- src/Participant/ParticipantRunner.ml
+++ src/Participant/ParticipantRunner.ml
@@ -1,11 +1,21 @@
 open Lwt.Infix
 module IrminLogMem = Ezirmin.FS_log(Tc.String)
 
-let run = Lwt_main.run
+let run = Lwt_main.run;;
+let write value = Lwt_io.write Lwt_io.stdout value;;
+let read () = Lwt_io.read_line Lwt_io.stdin ;;
+
+let getRepoString () = write "\n\027[93mIs your destination log local or remote (l/r): \027[39m" >>= fun _ ->
+  read () >>= function 
+    | "r" -> write "Destination Username: " >>= fun _ ->
+      read() >>= fun user ->
+      write "Destination Hostname: " >>= fun _ ->
+      read() >>= fun host ->
+      Lwt.return @@ Printf.sprintf "git+ssh://%s@%s/tmp/ezirmin/mempool" user host
+    | _ -> Lwt.return "/tmp/ezirminl/mempool"
 
 (*For remote repos...*)
-let root = run (Lwt_io.write Lwt_io.stdout "\n\027[93mPlease input the destination repo string: \027[39m" >>= fun _ ->
-  Lwt_io.read_line Lwt_io.stdin)
+let root = run @@ getRepoString()
 let memPoolMasterBranch = Lwt_main.run (IrminLogMem.init ~root:root ~bare: true () >>= IrminLogMem.master)
 let path = []
 
@@ -16,19 +26,19 @@ let addTransactionToMemPool senderID receiverID bookID =
   IrminLogMem.merge wipBranch ~into:memPoolMasterBranch;;
 
 let getLogEntryTuple () = 
-  Lwt_io.write Lwt_io.stdout "\n\027[39mYour ID (sender): \027[39m" >>= fun _ ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun senderID ->
-  Lwt_io.write Lwt_io.stdout "\027[39mTheir ID (receiver): \027[39m" >>= fun _ ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun receiverID ->
-  Lwt_io.write Lwt_io.stdout "\027[39mItem ID (book): \027[39m" >>= fun _ ->
-  Lwt_io.read_line Lwt_io.stdin >>= fun bookID ->
+  write "\n\027[39mYour ID (sender): \027[39m" >>= fun _ ->
+  read() >>= fun senderID ->
+  write "\027[39mTheir ID (receiver): \027[39m" >>= fun _ ->
+  read() >>= fun receiverID ->
+  write "\027[39mItem ID (book): \027[39m" >>= fun _ ->
+  read() >>= fun bookID ->
   Lwt.return (senderID, receiverID, bookID)
 
 let rec startParticipant () = 
-  Lwt_io.write_line Lwt_io.stdout "\027[35m-----------------------------------\n-----Enter Transaction Details-----\027[39m" >>= fun _ ->
+  write "\027[35m-----------------------------------\n-----Enter Transaction Details-----\027[39m" >>= fun _ ->
   getLogEntryTuple() >>= fun (sender, receiver, book) ->
   addTransactionToMemPool sender receiver book >>= fun _ ->
-  Lwt_io.write_line Lwt_io.stdout "\n\027[32mITEM ADDED SUCCESSFULLY\n" >>= 
+  write "\n\027[32mITEM ADDED SUCCESSFULLY\n" >>= 
   startParticipant;;
 
 run @@ startParticipant();;
