87a558ec46678dc635eaf3d107f4073b231cf18f
Charlie Crisp <charliecrisp249@gmail.com>
2018-02-16 11:40:10 +0000

    Refactor Participant

--- src/Participant/Participant.ml
+++ src/Participant/Participant.ml
@@ -18,24 +18,18 @@ end
 module Make(Config: I_ParticipantConfig)(LogCoder: I_LogStringCoder): I_Participant with type t = LogCoder.t = struct 
   type t = LogCoder.t
   module IrminLog = Ezirmin.FS_log(Tc.String)
-  let run = Lwt_main.run
-  let pull = IrminLog.Sync.pull
-  let root = "/tmp/ezirminl/part/mempool"
-  let mempool_master_branch = Lwt_main.run (IrminLog.init ~root:root ~bare:true () >>= IrminLog.master)
-  let path = []
+  let mempool_master_branch = Lwt_main.run (IrminLog.init ~root:"/tmp/ezirminl/part/mempool" ~bare:true () >>= IrminLog.master)
   let remote = IrminLog.Sync.remote_uri Config.leader_uri
 
-  let add_local_message_to_mempool message =
-    IrminLog.append ~message:"Entry added to the blockchain" mempool_master_branch ~path:path message 
-
   let add_transaction_to_mempool value =
+    let add_local_message_to_mempool message =
+      IrminLog.append ~message:"Entry added to the blockchain" mempool_master_branch ~path:[] message in
     let message = LogCoder.encode_string value in 
     match Config.is_local with
       | true -> add_local_message_to_mempool message >>= fun _ -> Lwt.return `Ok
       | false -> Lwt.catch 
-        (fun _ -> pull remote mempool_master_branch `Merge) 
+        (fun _ -> IrminLog.Sync.pull remote mempool_master_branch `Merge) 
         (fun _ -> Lwt.return `Error) >>= (function
           | `Ok -> add_local_message_to_mempool message >>= fun _ -> Lwt.return `Ok
           | _ -> Lwt.return `Could_Not_Pull_From_Remote)
 end
-
