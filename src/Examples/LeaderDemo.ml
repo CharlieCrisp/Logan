da4cd0812df11d084f4a6977f8a4cc285659bd69
Charlie Crisp <charliecrisp249@gmail.com>
2018-02-17 23:06:25 +0000

    Use cli arguments for remotes

--- src/Examples/LeaderDemo.ml
+++ src/Examples/LeaderDemo.ml
@@ -1,5 +1,10 @@
-module Remote : Blockchain.Remotes= struct
-  let remotes = []
+let format_remote str = let value = Printf.sprintf "git+ssh://%s/tmp/ezirminl/part/mempool" str in
+  Printf.printf "Using remote: %s\n%!" value; value
+
+let remotes = List.map format_remote (List.tl (Array.to_list Sys.argv))
+
+module Remote : Blockchain.Remotes = struct
+  let remotes = remotes
 end
 
 module Leader = Blockchain.Leader(Remote);;
