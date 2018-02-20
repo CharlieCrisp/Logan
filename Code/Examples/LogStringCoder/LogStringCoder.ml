d21b2d7cb52deac65ea5eb016e51baa1b95c57c5
Charlie Crisp <charliecrisp249@gmail.com>
2018-02-20 22:19:37 +0000

    Remove unnecessary library

--- Code/Examples/LogStringCoder/LogStringCoder.ml
+++ Code/Examples/LogStringCoder/LogStringCoder.ml
@@ -1,17 +1,17 @@
 module BookLogStringCoder: Blockchain.I_LogStringCoder with type t = string * string * string = struct
-  open Core_extended.Std
+  
   type t = string * string * string 
 
   type log_item = 
-      { time: Time.t;
+      { time: float;
         sender_id: string;
         receiver_id: string;
         book_id: string }
 
   let build_json ((sender_id, receiver_id, book_id):t) = 
     let str = Ezjsonm.string in 
-    let now = Time.to_string (Time.now()) in 
-    let time = ("Time", str now) in
+    let now = Unix.time() in 
+    let time = ("Time", str (string_of_float now)) in
     let send = ("sender_id", str sender_id) in 
     let receiver = ("receiver_id", str receiver_id) in
     let book = ("book_id", str book_id) in
