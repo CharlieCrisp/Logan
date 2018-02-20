df823696f14816180f61ea0c34fdcc5b2576460c
Charlie Crisp <charliecrisp249@gmail.com>
2018-02-20 22:35:04 +0000

    Fix remote_setup.sh problems

--- Utils/remote_setup.sh
+++ Utils/remote_setup.sh
@@ -2,7 +2,7 @@
 PURPLE='\033[1;35m'
 NC='\033[0m'
 
-OPAM_DEPENDS="ocamlfind ounit re jbuilder lwt core_extended"
+OPAM_DEPENDS="ocamlfind ounit re jbuilder lwt ezirmin"
 	 
 echo "yes" | sudo add-apt-repository ppa:avsm/ppa
 sudo apt-get update -qq
@@ -10,6 +10,7 @@ sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam m4 zlib1
 
 export OPAMYES=1
 opam init 
+opam switch 4.05.0
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
 
