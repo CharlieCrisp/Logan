5c689abd72df2ba69099211faf96f30ed4f268c1
Charlie Crisp <charliecrisp249@gmail.com>
2018-02-23 16:19:36 +0000

    Update sync_git to clone internal branch

--- Utils/sync_git.sh
+++ Utils/sync_git.sh
@@ -5,4 +5,8 @@ echo "Please give me something to sync from, in the form user@host"
 else
 LOCATION="ssh://$DESTINATION/tmp/ezirminl/lead/mempool"
 git clone $LOCATION /tmp/ezirminl/part/mempool
+cd /tmp/ezirminl/part/mempool
+git checkout -b internal origin/internal
+git checkout master
+cd -
 fi
