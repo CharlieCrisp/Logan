3567630fe3cdbed08892e40609a6baf16514aca9
Charlie Crisp <charliecrisp249@gmail.com>
2018-02-23 15:52:12 +0000

    Update sync_git to accept CLI arguments

--- Utils/sync_git.sh
+++ Utils/sync_git.sh
@@ -1,4 +1,8 @@
-read -p "Enter your destination username: " username
-read -p "Enter your destination ip: " ip
-LOCATION="ssh://$username@$ip/tmp/ezirminl/lead/mempool"
+DESTINATION=$1
+if [ -z "$DESTINATION" ]
+then
+echo "Please give me something to sync from, in the form user@host" 
+else
+LOCATION="ssh://$DESTINATION/tmp/ezirminl/lead/mempool"
 git clone $LOCATION /tmp/ezirminl/part/mempool
+fi
