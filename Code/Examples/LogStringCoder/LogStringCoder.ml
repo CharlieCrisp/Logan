1ba08f488a0e2cf17b6205abf477199973003f56
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-01 12:32:25 +0000

    Add TestLogStringCoder

--- Code/Examples/LogStringCoder/LogStringCoder.ml
+++ Code/Examples/LogStringCoder/LogStringCoder.ml
@@ -37,3 +37,40 @@ module BookLogStringCoder: Blockchain.I_LogStringCoder with type t = string * st
     with 
       | _ -> None
 end
+
+module TestLogStringCoder: Blockchain.I_LogStringCoder with type t = string * string = struct
+  
+  type t = string * string 
+
+  type log_item = 
+      { time: float;
+        machine_id: string;
+        txn_id: string }
+
+  let build_json ((machine_id, txn_id):t) = 
+    let str = Ezjsonm.string in 
+    let now = Ptime.to_float_s (Ptime_clock.now()) in 
+    let time = ("Time", str (string_of_float now)) in
+    let machine = ("machine_id", str machine_id) in 
+    let txn = ("txn_id", str txn_id) in
+    Ezjsonm.dict [time;machine;txn]
+
+  let encode_string (value:t) = 
+    let json = build_json value
+    in Ezjsonm.to_string json
+
+  let rec get_value (dict:(string * Ezjsonm.value) list) desired_key = match dict with 
+    | [] -> ""
+    | ((key, `String v)::_) when key = desired_key -> v
+    | ((str, _)::xs) -> get_value xs desired_key
+
+  let decode_string str = 
+    try
+      let json = Ezjsonm.from_string str in
+      let dict = Ezjsonm.get_dict json in
+      let machine = get_value dict "machine_id" in
+      let txn = get_value dict "txn_id" in
+      Some((machine, txn):t)
+    with 
+      | _ -> None
+end
