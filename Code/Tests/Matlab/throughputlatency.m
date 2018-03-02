954bf9f0a9e8d5b7170c5c72044ebb96cb682b6b
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-02 22:23:15 +0000

    Use correct bin_size and fix other bugs

--- Code/Tests/Matlab/throughputlatency.m
+++ Code/Tests/Matlab/throughputlatency.m
@@ -7,25 +7,29 @@ latency_data = zeros(1, bins);
 throughput_data = zeros(1, bins);
 
 length = size(data,1);
-bins = 10;
+bins = 30;
 binsize = floor(length/bins);
 
 for i = 1:bins
-    total_latency = 0;
-    start_time = data(i * binsize, 1);
+    start_index = i * binsize;
     end_index = (i+1)*binsize;
     if (end_index > length)
         end_index = length;
     end
+    this_bin_size = end_index - start_index;
+    start_time = data(i * binsize, 1);
     end_time = data(end_index, 1);
-    throughput = binsize / (end_time - start_time);
+    
+    total_latency = 0;
+    throughput = this_bin_size / (end_time - start_time);
+   
     for j = 1:binsize
        index = (i-1) * binsize + j;
        latency = data(index,2) - data(index,1); 
        total_latency = total_latency + latency;
     end
     throughput_data(i) = throughput;
-    latency_data(i) = total_latency;
+    latency_data(i) = total_latency/this_bin_size;
 end
 
-plot(throughput_data, latency_data);
+scatter(throughput_data, latency_data);
