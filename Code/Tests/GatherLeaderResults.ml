43a3fbe8da5a5c4edbd1261d1b939aefba7bf627
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-02 10:27:34 +0000

    Fix incorrect output in results gatherer

--- Code/Tests/GatherLeaderResults.ml
+++ Code/Tests/GatherLeaderResults.ml
@@ -11,8 +11,8 @@ let mempool_repo = run @@ IrminLogMem.init ~root: "/tmp/ezirminl/part/mempool" ~
 let mempool_master_branch = Lwt_main.run @@ IrminLogMem.master mempool_repo
 
 let rec log_list list = match list with 
-    | (x::[]) -> Lwt.return @@ Logger.info (Printf.sprintf "%s%!" x)
-    | (x::xs) -> Lwt.return @@ Logger.info (Printf.sprintf "%s\n%!" x) >>= fun _ -> log_list xs
+    | (x::[]) -> Lwt.return @@ Logger.log x
+    | (x::xs) -> Lwt.return @@ Logger.log x >>= fun _ -> log_list xs
     | [] -> Lwt.return @@ ()
 
 let log_list () = Lwt.return @@ Logger.info "\n-----Start Block-----\n" >>= fun _ ->
@@ -21,6 +21,6 @@ let log_list () = Lwt.return @@ Logger.info "\n-----Start Block-----\n" >>= fun 
   Lwt.return @@ Logger.log "\n-----Start MemPo-----\n" >>= fun _ ->
   IrminLogMem.read_all mempool_master_branch [] >>= fun list ->
   log_list list >>= fun _ ->
-  Lwt.return @@ Logger.log "\n------End MemPo------\n\n"
+  Lwt.return @@ Logger.log "\n------End MemPo------\n\n";;
 
 Lwt_main.run @@ log_list();;
