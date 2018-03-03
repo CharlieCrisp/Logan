8fb58d25940451d9a70be81dd57f55b0b4d21ae8
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-03 15:03:35 +0000

    Create function to process data

---
+++ Code/Tests/Matlab/throughputlatencymultiplertt.m
@@ -0,0 +1,22 @@
+format long;
+data = importdata("../../../single_remote_worker.log");
+
+[tp, lat_mean, lat_dev] = get_processed_data(data);
+
+blue = [0, 0.4470, 0.7410];
+
+figure
+e = errorbar(tp, lat_mean, lat_dev, 'Color', blue);
+e.LineStyle = "none";
+e.Marker = 'x';
+
+X = [ones(result_length,1) (tp')];
+b = X \ (lat_mean');
+ycalc = X * b;
+hold on
+pl = plot(tp, ycalc, 'Color', blue);
+legend('Location', 'northwest');
+legend([e;pl], 'Data for rtt = 2.2ms', 'Linear regression fit for rtt=2.2ms');
+
+xlabel({"Throughput";"transactions s^{-1}";""});
+ylabel({"Latency";"s"});
