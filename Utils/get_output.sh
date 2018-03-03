6f9b24e1aa599a1af7d15f6568285da79910408a
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-03 14:21:04 +0000

    Add scp script for retrieving logs

---
+++ Utils/get_output.sh
@@ -0,0 +1,8 @@
+USER=$1
+HOST=$2 
+if [ -z "$USER" ] || [ -z "$HOST" ] ;
+then
+echo "Please give me something to sync from, in the form user host" 
+else
+LOCATION="$USER@$HOST:/$USER/PartIIProject/output.log"
+scp $LOCATION .
