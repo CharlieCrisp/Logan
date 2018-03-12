938044e7db4255d7dd972e2136f3ed3dc18b3b06
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-12 17:32:53 +0000

    Add exe for adding number of txns to blockchain

---
+++ Code/Tests/AddNumberToBlockchain.ml
@@ -0,0 +1,53 @@
+(*bin/lead -r remoteuser@remotehost -i 1 -s 2018031233300*)
+
+open Lwt.Infix
+open Ptime
+let remote_uri = ref None
+let itr = ref 0
+let start_time = ref (Ptime_clock.now())
+let parse_is_local str = 
+  remote_uri := Some(str);
+  Printf.printf "\n\027[93mUsing leader address:\027[39m %s\n%!" str
+let parse_itr num = 
+  itr := num
+let parse_time input = 
+  let year = int_of_string (String.sub input 0 4) in
+  let month = int_of_string (String.sub input 4 2) in
+  let day = int_of_string (String.sub input 6 2) in
+  let hour = int_of_string (String.sub input 8 2) in
+  let min = int_of_string (String.sub input 10 2) in 
+  let sec = 0 in
+  let time: Ptime.time = ((hour, min, sec),0) in 
+  let date: Ptime.date = (year, month, day) in
+  match Ptime.of_date_time (date, time) with
+  | Some(new_date) -> start_time := new_date;
+  | _ -> ()
+
+let remote_tuple = ("-r", Arg.String parse_is_local, "Specify the remote repository in the form user@host")
+let itr_tuple = ("-n", Arg.Int parse_itr, "Specify the number of transactions you'd like to add to the blockchain")
+let start_tuple = ("-s", Arg.String parse_time, "Specify when this should begin")
+
+let _ = Arg.parse [remote_tuple; itr_tuple; start_tuple] (fun _ -> ()) ""
+
+type transaction = string * string * float
+module Config : Blockchain.I_ParticipantConfig with type t = transaction = struct 
+  type t = transaction
+  module LogCoder = LogStringCoder.TestLogStringCoder
+  let leader_uri = !remote_uri
+  let validator = None
+end
+
+module Participant = Blockchain.MakeParticipant(Config)
+
+let rec add_transactions = function
+  | 0 -> Lwt.return ()
+  | n -> Participant.add_transaction_to_mempool ("machine", "txn", 1.0) >>= fun _ ->
+    add_transactions (n-1)
+
+let rec test_blockchain_start() = 
+  let now = Ptime_clock.now() in 
+  match now > !start_time with  
+    | true -> add_transactions !itr
+    | false -> Lwt_unix.sleep 0.1 >>= fun _ -> test_blockchain_start();;
+
+Lwt_main.run @@ test_blockchain_start();;
--- Makefile
+++ Makefile
@@ -1,26 +1,8 @@
 all:
 	@jbuilder build Code/Examples/LeaderDemo.exe
 	@jbuilder build Code/Examples/ParticipantDemo.exe
-	@jbuilder build Code/Tests/TestLeader.exe
-	@jbuilder build Code/Tests/ThroughputLatencyTester.exe
-	@jbuilder build Code/Tests/GatherLeaderResults.exe
-	@jbuilder build Code/Tests/GatherNoRemResults.exe
 	@mkdir -p bin
 	@mv _build/default/Code/Examples/LeaderDemo.exe bin/lead.exe
-	@mv _build/default/Code/Examples/ParticipantDemo.exe bin/part.exe
-	@mv _build/default/Code/Tests/ThroughputLatencyTester.exe bin/ThroughputLatencyTester.exe
-	@mv _build/default/Code/Tests/TestLeader.exe bin/TestLeader.exe
-	@mv _build/default/Code/Tests/GatherLeaderResults.exe bin/GatherLeaderResults.exe
-	@mv _build/default/Code/Tests/GatherNoRemResults.exe bin/GatherNoRemResults.exe
-
-lead:
-	@jbuilder build Code/Examples/LeaderDemo.exe
-	@mkdir -p bin
-	@mv _build/default/Code/Examples/LeaderDemo.exe bin/lead.exe
-
-part:
-	@jbuilder build Code/Examples/ParticipantDemo.exe
-	@mkdir -p bin
 	@mv _build/default/Code/Examples/ParticipantDemo.exe bin/part.exe
 
 test:
@@ -28,11 +10,13 @@ test:
 	@jbuilder build Code/Tests/ThroughputLatencyTester.exe
 	@jbuilder build Code/Tests/GatherLeaderResults.exe
 	@jbuilder build Code/Tests/GatherNoRemResults.exe
+	@jbuilder build Code/Tests/AddNumberToBlockchain.exe
 	@mkdir -p bin
 	@mv _build/default/Code/Tests/ThroughputLatencyTester.exe bin/ThroughputLatencyTester.exe
 	@mv _build/default/Code/Tests/TestLeader.exe bin/TestLeader.exe
 	@mv _build/default/Code/Tests/GatherLeaderResults.exe bin/GatherLeaderResults.exe
 	@mv _build/default/Code/Tests/GatherNoRemResults.exe bin/GatherNoRemResults.exe
+	@mv _build/default/Code/Tests/AddNumberToBlockchain.exe bin/AddNumberToBlockchain.exe
 
 clean:
 	@jbuilder clean
