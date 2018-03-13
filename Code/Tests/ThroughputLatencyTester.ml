6d0eed71456bf2c64070f80e3b05b691601c7710
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-13 13:05:40 +0000

    Use shorter test time

--- Code/Tests/ThroughputLatencyTester.ml
+++ Code/Tests/ThroughputLatencyTester.ml
@@ -66,7 +66,7 @@ let rec test_blockchain_increasing inc_time rates =
 
 let rates = List.map (fun x -> 1.0 /. x) [1.0;2.0;3.0;4.0;5.0;6.0;7.0;8.0;9.0;10.0]
 (*first arg is the rate, second is the test duration*)
-let rates = List.map (fun x -> (x, 200.0 *. x)) rates
+let rates = List.map (fun x -> (x, 10.0 *. x)) rates
 
 
 let rec test_blockchain_start() = 
