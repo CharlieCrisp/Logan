e1d63e4d87f4ca5a9ea0019bdb0541ae336e7f49
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-16 15:13:54 +0000

    Add delay argument to test function

--- Code/Tests/AddNumberToBlockchain.ml
+++ Code/Tests/AddNumberToBlockchain.ml
@@ -8,6 +8,7 @@ open Ptime
 let remote_uri = ref None
 let itr = ref 0
 let id = ref 0
+let delay = ref None
 let start_time = ref (Ptime_clock.now())
 let parse_is_local str = 
   remote_uri := Some(str);
@@ -28,13 +29,15 @@ let parse_time input =
   match Ptime.of_date_time (date, time) with
   | Some(new_date) -> start_time := new_date;
   | _ -> ()
-
+let parse_delay del = 
+  delay := Some(del)
 let remote_tuple = ("-r", Arg.String parse_is_local, "Specify the remote repository in the form user@host")
 let itr_tuple = ("-n", Arg.Int parse_itr, "Specify the number of transactions you'd like to add to the blockchain")
 let start_tuple = ("-s", Arg.String parse_time, "Specify when this should begin")
 let id_tuple = ("-i", Arg.Int parse_id, "Specify machine id")
+let delay_tuple = ("-d", Arg.Float parse_delay, "Specify delay between transactions")
 
-let _ = Arg.parse [remote_tuple; itr_tuple; id_tuple; start_tuple] (fun _ -> ()) ""
+let _ = Arg.parse [remote_tuple; itr_tuple; id_tuple; start_tuple; delay_tuple] (fun _ -> ()) ""
 
 type transaction = string * string * float
 module Config : Blockchain.I_ParticipantConfig with type t = transaction = struct 
@@ -45,16 +48,22 @@ module Config : Blockchain.I_ParticipantConfig with type t = transaction = struc
 end
 
 module Participant = Blockchain.MakeParticipant(Config)
+let print_status n = Printf.printf "Added %i transactions \r%!" (!itr - n)
 
 let rec add_transactions = function
-  | 0 -> Lwt.return ()
-  | n -> Participant.add_transaction_to_mempool (string_of_int(!id), "txn", 1.0) >>= fun _ ->
-    add_transactions (n-1)
+  | 0 -> Printf.printf "\n%!"; Lwt.return ()
+  | n -> Participant.add_transaction_to_mempool (string_of_int(!id), string_of_int(n), 1.0) >>= fun _ ->
+    (match !delay with 
+      | None -> print_status n;
+        add_transactions (n-1)
+      | Some(del) -> Lwt_unix.sleep del >>= fun _ ->
+        print_status n;
+        add_transactions(n-1))
+
 
 let rec test_blockchain_start() = 
   let now = Ptime_clock.now() in 
-  match now > !start_time with  
-    | true -> add_transactions !itr
-    | false -> Lwt_unix.sleep 0.1 >>= fun _ -> test_blockchain_start();;
+  if  now > !start_time then add_transactions !itr else
+  Lwt_unix.sleep 0.1 >>= fun _ -> test_blockchain_start();;
 
 Lwt_main.run @@ test_blockchain_start();;
