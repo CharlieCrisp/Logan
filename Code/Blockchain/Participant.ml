7c0f97e6622843da717c03f99b7bc9eabfaffd57
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-16 19:30:18 +0000

    Actually pull updates before adding to mempool

--- Code/Blockchain/Participant.ml
+++ Code/Blockchain/Participant.ml
@@ -49,7 +49,7 @@ module Make(Config: I_ParticipantConfig): I_Participant with type t = Config.t =
       IrminLogBlock.Sync.pull remote_block ib `Merge
     | _ -> Lwt.return `Error
 
-  let pull_mem = match remote_mem_opt with
+  let pull_mem() = match remote_mem_opt with
     | Some(remote_mem) -> 
       IrminLogMem.get_branch mempool_repo "internal" >>= fun ib ->
       IrminLogMem.Sync.pull remote_mem mempool_master_branch `Merge >>= fun _ ->
@@ -87,7 +87,7 @@ module Make(Config: I_ParticipantConfig): I_Participant with type t = Config.t =
     match Config.leader_uri with
       | None -> add_local_message_to_mempool message >>= fun _ -> Lwt.return `Ok
       | _ -> Lwt.catch 
-        (fun _ -> Printf.printf "adding message"; pull_mem) 
+        (fun _ -> Printf.printf "adding message"; pull_mem()) 
         (fun _ -> Lwt.return `Error) >>= (function
           | `Ok -> add_local_message_to_mempool message >>= fun _ -> Lwt.return `Ok
           | _ -> Lwt.return `Could_Not_Pull_From_Remote)
