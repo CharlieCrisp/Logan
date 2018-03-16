224013336337630fd22221b53489a16a8b477f4c
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-16 17:35:03 +0000

    Fix bash script

--- Utils/get_output.sh
+++ Utils/get_output.sh
@@ -4,6 +4,6 @@ if [ -z "$USER" ] || [ -z "$HOST" ] ;
 then
 echo "Please give me something to sync from, in the form user host" 
 else
-LOCATION="$USER@$HOST:/$USER/PartIIProject/output.log"
+LOCATION="$USER@$HOST:/home/$USER/PartIIProject/output.log"
 scp $LOCATION .
 fi
