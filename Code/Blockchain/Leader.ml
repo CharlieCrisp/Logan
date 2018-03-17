855d626b95f1f4c56b04ae08c70ecbf7258875f4
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-17 15:46:44 +0000

    Add output when log item fails to reconstruct

--- Code/Blockchain/Leader.ml
+++ Code/Blockchain/Leader.ml
@@ -47,7 +47,7 @@ module Make (Config: I_Config) : I_Leader = struct
     match txn_opt with 
       | Some(txn) -> let txn_string = Config.LogCoder.encode_string txn in
         IrminLogBlock.append ~message:"Entry added to the blockchain" blockchain_master_branch ~path:path txn_string
-      | _ -> Lwt.return ()
+      | _ -> Printf.printf "Couldn't reconstruct log item"; Lwt.return ()
   let add_list_to_blockchain list = 
     Logger.info (Printf.sprintf "Starting to add to blockchain at time %f" (Ptime.to_float_s (Ptime_clock.now()))); 
     Lwt_list.iter_s add_txn_to_blockchain list >>= fun _ ->
