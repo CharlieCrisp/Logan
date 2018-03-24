332b72d2960d6a9b4c5d61ed83ff4c236e1b4932
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-24 12:45:40 +0000

    Update logic of PrintBlockchains

--- Code/Tests/PrintBlockchains.ml
+++ Code/Tests/PrintBlockchains.ml
@@ -1,33 +1,93 @@
+(*Get the size of the blockchain or of given mempools*)
 open Lwt.Infix
-
 module IrminLogBlock = Ezirmin.FS_log(Tc.String)
 module IrminLogMem = Ezirmin.FS_log(Tc.String)
 module IrminLogLeadMem = Ezirmin.FS_log(Tc.String)
 module Coder = LogStringCoder.TestLogStringCoder
+
+let blockchain = ref false
+let participant = ref false
+let mempools = ref []
+let f = ref None
+let l = ref None
+
+let print_blockchain = ("-b", Arg.Unit (fun () -> blockchain := true), "Print the blockchain")
+let print_participant = ("-p", Arg.Unit (fun () -> participant := true), "Print the local participant mempool")
+let print_first = ("-f", Arg.Int (fun num -> f := Some(num)), "Print only first n txns")
+let print_last = ("-l", Arg.Int (fun num -> l := Some(num)), "Print only last n txns")
+let print_mempools = ("-m", Arg.Rest (fun mempool -> mempools := ((Str.global_replace (Str.regexp "@") "" mempool)::(!mempools))), 
+  "Specify the remote mempools to print - in the format user@host")
+let _ = Arg.parse [print_blockchain;print_participant;print_first;print_last;print_mempools] (fun _ -> ()) ""
+
 let run = Lwt_main.run
 let path = []
-let blockchain_repo = run @@ IrminLogBlock.init ~root: "/tmp/ezirminl/lead/blockchain" ~bare:true ()
-let blockchain_master_branch = Lwt_main.run @@ IrminLogBlock.master blockchain_repo
-let mempool_repo = run @@ IrminLogMem.init ~root: "/tmp/ezirminl/part/mempool" ~bare:true ()
-let mempool_master_branch = Lwt_main.run @@ IrminLogMem.master mempool_repo
-let lead_mempool_repo = run @@ IrminLogLeadMem.init ~root: "/tmp/ezirminl/lead/mempool" ~bare:true ()
-let lead_mempool_master_branch = Lwt_main.run @@ IrminLogLeadMem.master lead_mempool_repo
 
-let rec print_list list = match list with 
-  | (x::[]) -> Lwt.return @@ Printf.printf "%s%!" x
-  | (x::xs) -> Lwt.return @@ Printf.printf "%s\n%!" x >>= fun _ -> print_list xs
-  | [] -> Lwt.return @@ ()
+let print_txn str = let txn = Coder.decode_string str in 
+  match txn with 
+    | Some((machine, txn, rate)) -> Printf.printf "Machine id: %s; Txn id: %s; Rate: %f\n%!" machine txn rate
+    | None -> Printf.printf "Could Not Decode!\n%!"
 
-let print_list () = Lwt.return @@ Printf.printf "\n\027[92m-----Start Block-----\027[32m\n" >>= fun _ ->
-  IrminLogBlock.read_all blockchain_master_branch [] >>= fun list1 ->
-  print_list list1 >>= fun _ ->
-  Lwt.return @@ Printf.printf "\n\027[93m-----Start MemPo-----\027[33m\n" >>= fun _ ->
-  IrminLogMem.read_all mempool_master_branch [] >>= fun list2 ->
-  print_list list2 >>= fun _ ->
-  Lwt.return @@ Printf.printf "\n\027[91m------End MemPo------\027[39m\n\n%!" >>= fun _ ->
-  IrminLogLeadMem.read_all lead_mempool_master_branch [] >>= fun list3 ->
-  print_list list3 >>= fun _ ->
-  Printf.printf "\n\027[91m------End MemPo------\027[39m\n\n%!";
-  Lwt.return @@Printf.printf "Blockchain: %i, Part Mempool: %i, Lead Mempool: %i\n%!" (List.length list1) (List.length list2) (List.length list3);;
+let rec print_list lst = match lst with 
+  | (x::[]) -> print_txn x
+  | (x::xs) -> print_txn x; print_list xs
+  | [] -> ()
 
-  Lwt_main.run @@ print_list();;
+let rec get_first num lst = match num, lst with 
+  | 0, _ -> []
+  | _, x::xs -> x::(get_first (num-1) xs)
+  | _, [] -> []
+
+let rec print_mempool_list repo = function 
+  | [] -> ()
+  | x::xs -> let branch = Lwt_main.run @@ IrminLogLeadMem.get_branch repo x in
+    let mempool_list = run @@ IrminLogLeadMem.read_all branch [] in
+    if mempool_list == [] then begin
+      Printf.printf "No items found\n%!";
+      print_mempool_list repo xs
+    end
+    else
+      match !f, !l with 
+        | None, None -> print_list mempool_list;
+          print_mempool_list repo xs
+        | Some(first), _ -> print_list (get_first first mempool_list);
+          print_mempool_list repo xs
+        | None, Some(last) -> print_list (get_first last (List.rev mempool_list));
+          print_mempool_list repo xs
+
+let rec print_local_mempool () = 
+  let repo = run @@ IrminLogMem.init ~root: "/tmp/ezirminl/part/mempool" ~bare:true () in 
+  let master_branch = Lwt_main.run @@ IrminLogMem.master repo in
+  let mempool_list = run @@ IrminLogLeadMem.read_all master_branch [] in
+  if mempool_list == [] then 
+      Printf.printf "No items found\n%!"
+  else
+  match !f, !l with 
+    | None, None -> print_list mempool_list
+    | Some(first), _ -> print_list (get_first first mempool_list)
+    | None, Some(last) -> print_list (get_first last (List.rev mempool_list))
+
+let rec print_local_blockchain () = 
+  let repo = run @@ IrminLogBlock.init ~root:"/tmp/ezirminl/lead/blockchain" ~bare:true () in
+  let master_branch = Lwt_main.run @@ IrminLogMem.master repo in
+  let blockchain_list = run @@ IrminLogLeadMem.read_all master_branch [] in
+  if blockchain_list == [] then 
+      Printf.printf "No items found\n%!"
+  else
+  match !f, !l with 
+    | None, None -> print_list blockchain_list
+    | Some(first), _ -> print_list (get_first first blockchain_list)
+    | None, Some(last) -> print_list (get_first last (List.rev blockchain_list))
+
+let print () = 
+  if !blockchain then begin
+    print_local_blockchain ()
+  end;
+  if !participant then begin
+    print_local_mempool ()
+  end;
+  if !mempools != [] then begin
+    let mempool_repo = run @@ IrminLogLeadMem.init ~root:"/tmp/ezirminl/lead/mempool" ~bare:true () in
+    print_mempool_list mempool_repo !mempools
+  end;;
+
+print();;
