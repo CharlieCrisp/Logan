885db36078d446c4c35cf15b77bf629ba489045d
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-24 09:07:11 +0000

    Don't use OCaml 4.05.0

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,7 +2,7 @@
 PURPLE='\033[1;35m'
 NC='\033[0m'
 
-OPAM_DEPENDS="ocamlfind ounit re=1.7.1 cppo=1.6.2 jbuilder=1.0+beta17 lwt=3.2.1 core core_extended ezirmin=0.2.1 git-unix=1.7.1"
+OPAM_DEPENDS="ocamlfind ounit re=1.7.1 cppo=1.6.2 jbuilder=1.0+beta17 lwt=3.2.1 conduit=0.15.4 core core_extended ezirmin=0.2.1 git-unix=1.7.1"
 	 
 echo "yes" | sudo add-apt-repository ppa:avsm/ppa
 sudo apt-get update -qq
@@ -10,7 +10,6 @@ sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam m4
 
 export OPAMYES=1
 opam init 
-opam switch 4.05.0
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
 
