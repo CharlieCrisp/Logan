f075a40a2a20cd97e4ebef939fc502d8f9d8a81a
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-26 14:47:14 +0100

    Add git and irmin pull comparison

--- Code/Tests/Matlab/latencysizeremote.m
+++ Code/Tests/Matlab/latencysizeremote.m
@@ -3,7 +3,9 @@ format long;
 data = importdata("~/Documents/PartIILogs/remote_worker_latency_size2.log", ' ');
 %filter NaN rows
 data(any(isnan(data), 2), :) = [];
-
+git_bad_latency = [1.441 1.637 1.644 1.706 1.751];
+irmin_git_bad_latency = [2.497 3.727 4.619 5.581 6.437];
+git_sizes = [ 0 1000 2000 3000 4000];
 
 %calculate latencies
 result = data(:,2) - data(:,1);
@@ -28,8 +30,10 @@ transactions = n*(1:length(result));
 plot(transactions, result);
 xlabel("Blockchain Size, transactions");
 ylabel("Latency, s");
-ylim([1.5 15.0]);
-
+ylim([0 15.0]);
+hold on
+plot(git_sizes, irmin_git_bad_latency);
+legend('Logan transaction latency','Irmin Pull Latency')
 %least mean squares plot
 % X = [ones(length(result),1) (transactions')];
 % size(X)
@@ -38,3 +42,8 @@ ylim([1.5 15.0]);
 % ycalc = X * b;
 % hold on
 % plot(transactions, ycalc);
+hold off
+figure
+irmin_add_bad = [5.161 5.585 10.438 15.439 18.00];
+irmin_sizes = [ 0 1000 2000 3000 4000];
+plot(irmin_sizes, irmin_add_bad);
