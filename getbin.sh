d0fabdf29ec1791b34b492da815bd011713d3d41
Charlie Crisp <charliecrisp249@gmail.com>
2018-03-27 20:39:04 +0000

    Add getbin script

---
+++ getbin.sh
@@ -0,0 +1,14 @@
+cd ~/Logan/
+mkdir bin
+cd bin/
+scp root@23.253.159.211:/root/Logan/bin/GetSize .
+scp root@23.253.159.211:/root/Logan/bin/AddNumberToBlockchain  .
+scp root@23.253.159.211:/root/Logan/bin/PrintBlockchains .
+scp root@23.253.159.211:/root/Logan/bin/TestLeader .
+scp root@23.253.159.211:/root/Logan/bin/lead .
+scp root@23.253.159.211:/root/Logan/bin/part .
+scp root@23.253.159.211:/root/Logan/bin/ThroughputLatencyTester .
+scp root@23.253.159.211:/root/Logan/bin/TestAddLatency .
+scp root@23.253.159.211:/root/Logan/bin/OCamlGitPull .
+scp root@23.253.159.211:/root/Logan/bin/GatherResults .
+cd -
