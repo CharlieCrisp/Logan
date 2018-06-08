8a2ae02d0a1aabb790c05c66b048c1a31a97f59d
Charlie Crisp <charliecrisp249@gmail.com>
2018-06-08 10:14:44 +0100

    Tidy up repo by removing MATLAB files

--- Code/Tests/Matlab/git_latency.m
+++
@@ -1,5 +0,0 @@
-filename = '../git_pull_latency.txt';
-delimiterIn = ' ';
-headerlinesIn = 1;
-A = importdata(filename,delimiterIn,headerlinesIn);
-plot(A.data(:,1),A.data(:,2), A.data(:,1), A.data(:,3))
--- Code/Tests/Matlab/gitlatencysizepull.m
+++
@@ -1,9 +0,0 @@
-% compare git latencies for different size pulls 
-figure
-data_git = importdata('~/Documents/PartIILogs/git_pulls_different_size_pull.log', ' ');
-data_ezirmin = importdata('~/Documents/PartIILogs/ezirmin_pulls_different_size_pulls.log', ' ');
-plot((0:length(data_git)-1 )* 100, data_git, (0:length(data_ezirmin)-1)* 100, data_ezirmin);
-lgd = legend("Git Pull", "Ezirmin Pull");
-lgd.Location = 'northwest';
-xlabel("Number of transactions pulled");
-ylabel("Latency, s");
